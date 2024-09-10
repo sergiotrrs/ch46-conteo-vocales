8341300948f34c304d0be38ce6651e3eb485f7ed
Sergio Torres <sergio.torres@idr.edu.mx>
2024-09-10 12:28:39 -0600

    feat: funcion que maneja el input nombre

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log("Conteo de vocales");
+
+/**
+ Encuentra el número de letras que existen en el
+ nombre indicado.
+ @letra {string} letra a buscar
+ @nombre {string} texto a buscar la letra
+ return número de letras encontradas
+*/
+function encontrarNumeroDeLetra( letra, nombre ) {    
+    let numeroDeLetras = 0;
+    for(let index = 0; index < nombre.length; index++ ) {
+        let letraAEvaluar = nombre.charAt( index );
+        if( letraAEvaluar === letra){
+            numeroDeLetras++;
+        }
+    }
+    return numeroDeLetras;
+}
+
+function manejoDelEventoDeInputNombre(){
+   console.log("Me estoy invocando");
+}
+
+
